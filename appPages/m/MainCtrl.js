5b34e0adf6e11794e6e9fe9367a0f529ee139337
kevin <iscarryon@gmail.com>
2015-07-15 00:28:56 +0800

    kevin: change js content v11

--- appPages/m/MainCtrl.js
+++ appPages/m/MainCtrl.js
@@ -1,100 +1,93 @@
 ﻿app.controller('MainCtrl', ['$scope', '$rootScope', '$global', '$state', 'User', 'ngDialog','CustomersFactory','VouSaleFactory','OrdersFactory',
     function ($scope, $rootScope, $global, $state, User, ngDialog,CustomersFactory,VouSaleFactory,OrdersFactory) {
-        alert("in m");
+        
 
         $("#loading").fadeOut("fast");
+
         $scope.vm = {};
         $scope.autocomplete = {};
 
-        // $scope.$on('updateCartCount', function (e, data) {
-        //     $scope.vm.cartNo = $global.cart.length;
-        // });
+        $scope.$on('updateCartCount', function (e, data) {
+            $scope.vm.cartNo = $global.cart.length;
+        });
 
-        // $scope.vm.createNewOrder = function () {
-        //     ngDialog.open({
-        //         scope: $scope,
-        //         template: 'selectCustomer.html'
-        //     });
-        // };
+        $scope.vm.createNewOrder = function () {
+            ngDialog.open({
+                scope: $scope,
+                template: 'selectCustomer.html'
+            });
+        };
 
-        // //autocomplete 客戶 輸入字串時動作
-        // $scope.autocomplete.searchCustomer = function (typedthings) {
-        //     if (typedthings == '' && typeof $scope.vm.autocompleteSelect == 'undefined') {
-        //         return;
-        //     }
-        //     $scope.autocomplete.isSearchCustomer = true;
-        //     CustomersFactory.getByName(typedthings).success(function (data) {
-        //         if (data.length == 0) {
-        //             $scope.autocomplete.isSearchCustomer = false;
-        //             return;
-        //         }
-        //         CustomersFactory.getAutoCompleteOptions(data, typedthings).then(function (item) {
-        //             $scope.autocomplete.allCustomersName = item;
-        //             $scope.autocomplete.isSearchCustomer = false;
-        //         });
-        //     }).error(function () {
-        //         $scope.autocomplete.isSearchCustomer = false;
-        //     });
-        // };
+        //autocomplete 客戶 輸入字串時動作
+        $scope.autocomplete.searchCustomer = function (typedthings) {
+            if (typedthings == '' && typeof $scope.vm.autocompleteSelect == 'undefined') {
+                return;
+            }
+            $scope.autocomplete.isSearchCustomer = true;
+            CustomersFactory.getByName(typedthings).success(function (data) {
+                if (data.length == 0) {
+                    $scope.autocomplete.isSearchCustomer = false;
+                    return;
+                }
+                CustomersFactory.getAutoCompleteOptions(data, typedthings).then(function (item) {
+                    $scope.autocomplete.allCustomersName = item;
+                    $scope.autocomplete.isSearchCustomer = false;
+                });
+            }).error(function () {
+                $scope.autocomplete.isSearchCustomer = false;
+            });
+        };
 
-        // //autocomplete 客戶 選擇動作
-        // $scope.autocomplete.assignCustomer = function (suggestion) {
-        //     CustomersFactory.getByName(suggestion).success(function (customer) {
-        //         var aa = _.find(customer, function (item) {
-        //             return item.CUST_NAME == suggestion;
-        //         });
-        //         $scope.vm.autocompleteSelect = aa.CUST_NAME;    // only customer name
-        //         $scope.vm.selectedCustomer = aa; // customer obj
-        //     }).error();
-        // };
+        //autocomplete 客戶 選擇動作
+        $scope.autocomplete.assignCustomer = function (suggestion) {
+            CustomersFactory.getByName(suggestion).success(function (customer) {
+                var aa = _.find(customer, function (item) {
+                    return item.CUST_NAME == suggestion;
+                });
+                $scope.vm.autocompleteSelect = aa.CUST_NAME;    // only customer name
+                $scope.vm.selectedCustomer = aa; // customer obj
+            }).error();
+        };
 
-        // $scope.vm.addToCustomerToCart = function () {
-        //     if ($scope.vm.autocompleteSelect) {
-        //         var ppObj = {
-        //             CUST_ID: $scope.vm.selectedCustomer.CUST_ID,
-        //             SALE_ID: typeof $global.loginUser.EMP_ID == 'undefined' ? null : $global.loginUser.EMP_ID
-        //         };
+        $scope.vm.addToCustomerToCart = function () {
+            if ($scope.vm.autocompleteSelect) {
+                var ppObj = {
+                    CUST_ID: $scope.vm.selectedCustomer.CUST_ID,
+                    SALE_ID: typeof $global.loginUser.EMP_ID == 'undefined' ? null : $global.loginUser.EMP_ID
+                };
 
-        //         VouSaleFactory.getNewVouSaleByOrdMaster().success(function (obj) {
-        //             ppObj.VOU_SALE = obj;
-        //             $global.newOrder = OrdersFactory.preparePostObj(ppObj);
-        //             $global.selectedCustomer = $scope.vm.selectedCustomer;
+                VouSaleFactory.getNewVouSaleByOrdMaster().success(function (obj) {
+                    ppObj.VOU_SALE = obj;
+                    $global.newOrder = OrdersFactory.preparePostObj(ppObj);
+                    $global.selectedCustomer = $scope.vm.selectedCustomer;
 
-        //             // 還沒選產品
-        //             //var newItem = OrderDetailsFactory.preparePostObj($global.selectedOrder, data);
 
-        //             //$global.addToCart(newItem);
+                    $state.go('m.Products.category');
+                }).error(function (err) {
+                    console.log(err);
+                });
 
-        //             // 更新購物車數量
-        //             //$scope.$emit('updateCartCount', '');
-
-        //             //跳到產品選擇畫面
-        //             $state.go('m.Products.category');
-        //         }).error(function (err) {
-        //             console.log(err);
-        //         });
-
-        //         ngDialog.close();
-        //     }
-        //     else {
-        //         alert("請選擇客戶");
-        //     }
-        // }
+                ngDialog.close();
+            }
+            else {
+                alert("請選擇客戶");
+            }
+        }
        
 
-        // $scope.vm.goToOrderForm = function () {
-        //     if ($global.newOrder == null) {
-        //         alert("請先新建訂單");
-        //         return;
-        //     }
-        //     $state.go('m.Orders.create');
-        // };
+        $scope.vm.goToOrderForm = function () {
+            if ($global.newOrder == null) {
+                alert("請先新建訂單");
+                return;
+            }
+            $state.go('m.Orders.create');
+        };
 
-        // $scope.vm.logout = function () {
-        //     $global.resetAllProperty();
-        //     User.removeAuthentication();
-        //     $state.go('login');
-        // };
+        $scope.vm.logout = function () {
+            $global.resetAllProperty();
+            User.removeAuthentication();
+            $state.go('login');
+        };
 
 
     }]);
--- appPages/mHome/index.js
+++ appPages/mHome/index.js
@@ -3,37 +3,34 @@ angular.module('app', []).controller('ListindexCtrl', [
     '$scope', '$state', '$rootScope',
     function ($scope, $state, $rootScope) {
 
-        alert("in m.home");
-        // 可取代icdtdata為對應model名稱，例: icdtdata --> Order
-
         // 宣告vm
         $scope.vm = {};
 
-        // 動作
-        // //產品與庫存管理
-        // $scope.goToProducts = function (obj) {
-        //     $state.go("m.Products.category");
-        // };
-        // //顧客關係
-        // $scope.goToCustomers = function (obj) {
-        //     $state.go("m.Customers.list");
-        // };
-        // //行程規劃
-        // $scope.goToEEvents = function (obj) {
-        //     $state.go("m.EEvents.plan");
-        // };
-        // //帳務管理
-        // $scope.goToUser = function (obj) {
-        //     $state.go("m.Orders.list");
-        // };
-        // //最新消息
-        // $scope.goToNews = function (obj) {
-        //     $state.go("m.News.list");
-        // };
-        // //設定
-        // $scope.goToSettings = function (obj) {
-        //     $state.go("m.Users.list");
-        // };
+        動作
+        //產品與庫存管理
+        $scope.goToProducts = function (obj) {
+            $state.go("m.Products.category");
+        };
+        //顧客關係
+        $scope.goToCustomers = function (obj) {
+            $state.go("m.Customers.list");
+        };
+        //行程規劃
+        $scope.goToEEvents = function (obj) {
+            $state.go("m.EEvents.plan");
+        };
+        //帳務管理
+        $scope.goToUser = function (obj) {
+            $state.go("m.Orders.list");
+        };
+        //最新消息
+        $scope.goToNews = function (obj) {
+            $state.go("m.News.list");
+        };
+        //設定
+        $scope.goToSettings = function (obj) {
+            $state.go("m.Users.list");
+        };
 
 
     }]);
