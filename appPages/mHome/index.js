8e161216ad273b719d473e4d22832ebc41e25fb5
kevin <iscarryon@gmail.com>
2015-07-15 00:19:10 +0800

    kevin: change js content v8

--- appPages/mHome/index.js
+++ appPages/mHome/index.js
@@ -9,30 +9,30 @@ angular.module('app', []).controller('ListindexCtrl', [
         $scope.vm = {};
 
         // 動作
-        //產品與庫存管理
-        $scope.goToProducts = function (obj) {
-            $state.go("m.Products.category");
-        };
-        //顧客關係
-        $scope.goToCustomers = function (obj) {
-            $state.go("m.Customers.list");
-        };
-        //行程規劃
-        $scope.goToEEvents = function (obj) {
-            $state.go("m.EEvents.plan");
-        };
-        //帳務管理
-        $scope.goToUser = function (obj) {
-            $state.go("m.Orders.list");
-        };
-        //最新消息
-        $scope.goToNews = function (obj) {
-            $state.go("m.News.list");
-        };
-        //設定
-        $scope.goToSettings = function (obj) {
-            $state.go("m.Users.list");
-        };
+        // //產品與庫存管理
+        // $scope.goToProducts = function (obj) {
+        //     $state.go("m.Products.category");
+        // };
+        // //顧客關係
+        // $scope.goToCustomers = function (obj) {
+        //     $state.go("m.Customers.list");
+        // };
+        // //行程規劃
+        // $scope.goToEEvents = function (obj) {
+        //     $state.go("m.EEvents.plan");
+        // };
+        // //帳務管理
+        // $scope.goToUser = function (obj) {
+        //     $state.go("m.Orders.list");
+        // };
+        // //最新消息
+        // $scope.goToNews = function (obj) {
+        //     $state.go("m.News.list");
+        // };
+        // //設定
+        // $scope.goToSettings = function (obj) {
+        //     $state.go("m.Users.list");
+        // };
 
 
     }]);
